c0d91a4c7a39ca801952959b630a96c5918e8fc7
Letaryat <69041891+Letaryat@users.noreply.github.com>
2025-03-21 20:25:01 +0100

    1

---
+++ CS2-SurfFixTeamFull.cs
@@ -0,0 +1,33 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Utils;
+
+namespace cs2_surffixteamfull;
+
+public class cs2_surffixteamfull : BasePlugin
+{
+    public override string ModuleName => "CS2-SurfFixTeamFull";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override string ModuleAuthor => "Letaryat";
+    public override string ModuleDescription => "TEAM FULL FIX YIPEEEEEE";
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("Team full fix on");
+        RegisterEventHandler<EventJointeamFailed>((e, i) =>
+        {
+            var player = e.Userid;
+            if (player == null || player.IsBot || player.IsHLTV) return HookResult.Continue;
+            player.SwitchTeam(CsTeam.CounterTerrorist);
+            return HookResult.Continue;
+        });
+    }
+    public override void Unload(bool hotReload)
+    {
+        Console.WriteLine("Team full fix off");
+    }
+
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CS2-SurfFixTeamFull
  
+Plugin for CS2 Surf / BH / KZ servers that fix if the team are "full". Simply just transfer player into CT after EventJoinTeamFailed.
